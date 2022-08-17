15fca5fe83674764ef78134f0cb46e72c2d7d6c7
Julien Noel <jncoweb@gmail.com>
2022-08-17 16:38:31 +0200

    maj gamescreen

--- Screens/gameScreen.js
+++ Screens/gameScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from "react-native";
 import { Card, Icon } from "@rneui/themed";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export function GameScreen(props) {
   const [exerciceList, setExerciceList] = useState([]);
@@ -13,31 +14,70 @@ export function GameScreen(props) {
 
   const displayExercice = exerciceList.map((el, i) => {
     return (
-    <View>
-      <Card containerStyle={{ marginTop: 15 }}>
-        <Card.Title>{el.exerciceId}</Card.Title>
-        <Card.Divider />
+    
+        
+    
+    <LinearGradient        
+        colors={['#9fa8da', '#6e7fd1', '#4f14b5']}
+        style={styles.card}
+        key={i}
+        
+      >
+        <View>
+            <Text style={{fontSize: 20, fontWeight: '300', color: 'white'}}>{el.exerciceId}</Text>
+        </View>
+        <View style={{height: '70%', justifyContent: 'center'}}>
         <Icon
         raised
         name='lock'
         type='font-awesome'
-        color='#f50'/>
-      </Card>
-      </View>
+        color='#f50'
+        size={30}
+        onPress={() => console.log('hello')}        
+        />
+        </View>
+        </LinearGradient>       
+      
+       
     );
   });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", flexDirection: 'row' }}>
+    
         <ScrollView showsVerticalScrollIndicator={false}>
+                    
+        <View style={styles.container}>
             {displayExercice}
+        </View>
         </ScrollView>
-    </View>
+    
   );
 }
 
 function mapStateToProps(state) {
   return { langue: state.languageSelect, exo: state.exercice };
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: '15%',
+        justifyContent: 'center'       
+    },
+    card: {                                              
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '10%',
+        width: '40%',        
+        margin: '3%',
+        borderRadius: 15,
+        
+                               
+    },
+    
+    
+  });
 
 export default connect(mapStateToProps, null)(GameScreen);
